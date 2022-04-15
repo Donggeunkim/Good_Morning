444203965fd9649fd52fd2d3add88475bb375505
Donggeun_Kim <elf1998@naver.com>
2022-04-15 09:24:27 +0900

    [feat] Main / test 기본 클래스 업로드
    최초 개발에 필요한 main / test 기본 클래스 업로드 건 입니다

---
+++ src/test/java/Main_test.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+public class Main_test {
+    @Test
+    void sum_test() {
+        assertEquals(5, 2 + 3);
+    }
+}

493506f190f5cdd0ba41fe34bf2ee20a9de1a53d
김태현 <103616675+ktaehyeon@users.noreply.github.com>
2022-04-18 10:56:51 +0900

    [feat] : EmployeeHandler Class 추가

--- src/main/java/Good_Morning/EmployeeHandler.java
+++ src/main/java/Good_Morning/EmployeeHandler.java
@@ -1,32 +1,83 @@
 package Good_Morning;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 
 public class EmployeeHandler {
-    public HashMap<String, Employee> employee;
+    public HashMap<String, Employee> employee_;
+    public ArrayList<String> search_result_;
 
-    public void addEmployee(String employeeNum, String name, String cl, String phoneNum, String birthday, String certi){
-        if (employee == null) employee = new HashMap<>();
-        Employee employeeData = new Employee(employeeNum, name, cl, phoneNum, birthday, certi);
-        employee.put(employeeNum, employeeData);
+    private ArrayList<Employee> SortEmployee(){
+        ArrayList<Employee> sort_result = new ArrayList<>();
+
+        for(String employee_num : search_result_){
+            sort_result.add(employee_.get(employee_num));
+        }
+
+        Collections.sort(sort_result, new Comparator<Employee>(){
+            public int compare(Employee o1, Employee o2) {
+                if(o1.getEmployedYear()<o2.getEmployedYear()) {
+                    return -1;
+                }else if(o1.getEmployedYear()>o2.getEmployedYear()) {
+                    return 1;
+                }else {
+                    if(o1.getEmployedYear()==o2.getEmployedYear()) {
+                        return o2.getEmployeeNum().compareTo(o1.getEmployeeNum());
+                    }else {
+                        return 0;
+                    }
+                }
+            }
+        });
+
+        return sort_result;
     }
 
-    public int searchEmployee(ArrayList<String> option, ArrayList<String> schItem){
-        SchCommand searchBase = new SchCommand();
-        ArrayList<String> employeeList = searchBase.executeJob(this.employee, option, schItem);
-        if(employeeList.size()==0) System.out.println("SCH,NONE");
+    public EmployeeHandler(){
+        employee_ = new HashMap<>();
+        search_result_ = new ArrayList<>();
+    }
+
+    public void AddEmployee(ArrayList<String> option, ArrayList<String> search_item){
+        AddCommand add_command = new AddCommand();
+        add_command.executeJob(employee_, option, search_item);
+    }
+
+    public int SearchEmployee(ArrayList<String> option, ArrayList<String> search_item){
+        SchCommand search_base = new SchCommand();
+        search_result_ = search_base.executeJob(this.employee_, option, search_item);
+
+        return search_result_.size();
+    }
+
+    public void DeleteEmployee(ArrayList<String> option, ArrayList<String> search_item){
+    }
+
+    public void ModifyEmployee(ArrayList<String> option, ArrayList<String> search_item){
+    }
+
+    // Del이나 Mod동작 SCH -> Print -> DEL or MOD ??
+    public void PrintEmployee(String Command, ArrayList<String> option, ArrayList<String> search_item){
+        if(search_result_.size()==0) System.out.println(Command + ",NONE");
         else {
             if (option.get(0).equals("p")) {
-                for (String employeeNum : employeeList) {
-                    System.out.println("SCH,"+ this.employee.get(employeeNum).getName());
+                ArrayList<Employee> sort_result = SortEmployee();
+
+                if (sort_result.size() > 5){
+                    sort_result = new ArrayList<>(sort_result.subList(0,5));
+                }
+
+                for (Employee employee : sort_result) {
+                    System.out.println(Command + "," + employee.getName());
                 }
 
             } else {
-                System.out.print("SCH," + employeeList.size());
+                System.out.println(Command + "," + search_result_.size());
             }
         }
-        return employeeList.size();
     }
+
 }
 

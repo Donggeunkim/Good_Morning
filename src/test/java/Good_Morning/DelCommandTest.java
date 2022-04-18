4c7f564c72265a7b35c6e607f509689df2616998
MINHWANKIM90 <kmhwan90@gmail.com>
2022-04-18 16:21:39 +0900

    [test] Add, Del Command Test Case 추가

---
+++ src/test/java/Good_Morning/DelCommandTest.java
@@ -0,0 +1,65 @@
+package Good_Morning;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class DelCommandTest {
+    EmployeeHandler employeeHandler;
+    ArrayList<String> option1;
+    ArrayList<String> parameter1;
+    @BeforeEach
+    void setup() {
+        employeeHandler = new EmployeeHandler();
+
+        option1 = new ArrayList<>();
+        option1.add(" ");
+        option1.add(" ");
+        option1.add(" ");
+
+        parameter1 = new ArrayList<>();
+        parameter1.add("14016092");
+        parameter1.add("KIM MINHWAN");
+        parameter1.add("CL3");
+        parameter1.add("010-2802-9011");
+        parameter1.add("19900109");
+        parameter1.add("PRO");
+    }
+
+    @Test
+    void delFuncTest(){
+        employeeHandler.addEmployee(option1, parameter1);
+
+        ArrayList<String> schItem = new ArrayList<>();
+        schItem.add("employeeNum");
+        schItem.add("14016092");
+
+        employeeHandler.deleteEmployee(option1, schItem);
+
+        assertEquals(0, employeeHandler.employee.size());
+    }
+
+    @Test
+    void delClSearchTest() throws IOException {
+        String filePath = "C:/education/input.txt";
+        ArrayList<String> commandLines = FileIo.readInputFile(filePath);
+
+        Parser parser = new Parser();
+        for(String commandLine : commandLines) {
+            parser.splitCommnadLine(commandLine);
+            employeeHandler.addEmployee(parser.getOptions(), parser.getValues());
+        }
+
+        ArrayList<String> schItem = new ArrayList<>();
+        schItem.add("cl");
+        schItem.add("CL2");
+
+        employeeHandler.deleteEmployee(option1, schItem);
+
+        assertEquals(66428, employeeHandler.employee.size());
+    }
+}

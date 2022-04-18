4c47598cbbd8d80917551c35cae7599b1b368c07
김태현 <103616675+ktaehyeon@users.noreply.github.com>
2022-04-18 19:48:21 +0900

    [fix] 기존 print 정렬 함수 제거

--- src/main/java/Good_Morning/EmployeeHandler.java
+++ src/main/java/Good_Morning/EmployeeHandler.java
@@ -6,32 +6,6 @@ public class EmployeeHandler {
     public TreeMap<String, Employee> employee;
     public ArrayList<String> searchResult;
 
-    private ArrayList<Employee> sortEmployee(){
-        ArrayList<Employee> sortResult = new ArrayList<>();
-
-        for(String employeeNum : searchResult){
-            sortResult.add(employee.get(employeeNum));
-        }
-
-        Collections.sort(sortResult, new Comparator<Employee>(){
-            public int compare(Employee o1, Employee o2) {
-                if(o1.getEmployedYear()<o2.getEmployedYear()) {
-                    return -1;
-                }else if(o1.getEmployedYear()>o2.getEmployedYear()) {
-                    return 1;
-                }else {
-                    if(o1.getEmployedYear()==o2.getEmployedYear()) {
-                        return o2.getEmployeeNum().compareTo(o1.getEmployeeNum());
-                    }else {
-                        return 0;
-                    }
-                }
-            }
-        });
-
-        return sortResult;
-    }
-
     public void addEmployee(ArrayList<String> options, ArrayList<String> parameters){
         AddCommand addCommand = new AddCommand(options, parameters);
         Employee employeeData = addCommand.ExecuteJob(this.employee, options, parameters);
@@ -68,7 +42,10 @@ public class EmployeeHandler {
         if(searchResult.size()==0) System.out.println(Command + ",NONE");
         else {
             if (option.get(0).equals("p")) {
-                ArrayList<Employee> sortResult = sortEmployee();
+                ArrayList<Employee> sortResult = new ArrayList<Employee>();
+                for(String employeeNum : searchResult){
+                    sortResult.add(this.employee.get(employeeNum));
+                }
 
                 if (sortResult.size() > 5){
                     sortResult = new ArrayList<>(sortResult.subList(0,5));
